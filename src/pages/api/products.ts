cfcfd9c7fb8aad41be87f591f6f17e2c991442c1
alyssariah <alyssariah@gmail.com>
2023-05-02 23:28:29 -0400

    staging

---
+++ src/pages/api/products.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  data?: string,
+  error?: string,
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await axios.get('https://api.bigcommerce.com/stores/jo3a1dt1/v3/catalog/products', {
+    headers: {
+      accept: 'application/json',
+      'X-Auth-Token': 'axkiw2t6o9us7bcafi83lxcdzl3f65t'
+    },
+    params: {
+      sku: 'THX-1138',
+      include: 'images' 
+    }
+  }).then(({data}) => {
+    res.status(200).json({ data: data.data }) 
+  }).catch(({error}) => {
+    res.status(400).json({ error })
+  })
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,102 @@
-const plugin = require('tailwindcss/plugin');
-const colors = require('tailwindcss/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './icons/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}'
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '500px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1920px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      'transparent': 'transparent',
+      'blue-50': 'rgb(231, 232, 234)',
+      'blue-100': 'rgb(179, 182, 189)',
+      'blue-200': 'rgb(143, 147, 157)',
+      'blue-300': 'rgb(92, 98, 113)',
+      'blue-400': 'rgb(60, 68, 85)',
+      'blue-500': 'rgb(11, 21, 43)',
+      'blue-600': 'rgb(10, 19, 39)',
+      'blue-700': 'rgb(8, 15, 31)',
+      'blue-800': 'rgb(6, 12, 24)',
+      'blue-900': 'rgb(5, 9, 18)',
+      'orange-50': 'rgb(254, 236, 232)',
+      'orange-100': 'rgb(252, 195, 182)',
+      'orange-200': 'rgb(250, 166, 147)',
+      'orange-300': 'rgb(248, 126, 98)',
+      'orange-400': 'rgb(246, 101, 68)',
+      'orange-500': 'rgb(244, 62, 21)',
+      'orange-600': 'rgb(222, 56, 19)',
+      'orange-700': 'rgb(173, 44, 15)',
+      'orange-800': 'rgb(134, 34, 12)',
+      'orange-900': 'rgb(102, 26, 9)',
+      'white-50': 'rgb(255, 255, 255)',
+      'white-100': 'rgb(254, 254, 255)',
+      'white-200': 'rgb(253, 253, 255)',
+      'white-300': 'rgb(252, 252, 255)',
+      'white-400': 'rgb(252, 252, 255)',
+      'white-500': 'rgb(251, 251, 255)',
+      'white-600': 'rgb(228, 228, 232)',
+      'white-700': 'rgb(178, 178, 181)',
+      'white-800': 'rgb(138, 138, 140)',
+      'white-9000': 'rgb(105, 105, 107)',
+    },
+    fontFamily: {
+      sans: ['Roberto', 'sans-serif'],
+    },
+    fontSize: {
+      'heading-lg': '2.441em',
+      'heading-md': '1.953em',
+      'heading-sm': '1.563em',
+      'body-lg': '1.25em',
+      'body-md': '1em',
+      'body-sm': '.8em'
+    },
+    fontWeight: {
+      'hairline': 100,
+      'extra-light': 100,
+      'thin': 200,
+      'light': 300,
+      'normal': 400,
+      'medium': 500,
+      'semibold': 600,
+      'bold': 700,
+      'extrabold': 800,
+      'extra-bold': 800,
+      'black': 900,
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '2px',
+      'DEFAULT': '4px',
+      'md': '6px',
+      'lg': '8px',
+      'xl': '12px',
+      '2xl': '16px',
+      '3xl': '24px',
+      'full': '9999px',
+      'large': '12px',
+    },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)']
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        gray: colors.neutral,
-        hotPink: '#FF1966',
-        dark: '#111111',
-        light: '#FAFAFA',
-        violetDark: '#4c2889'
-      },
-      keyframes: {
-        fadeIn: {
-          from: { opacity: 0 },
-          to: { opacity: 1 }
-        },
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
-        },
-        blink: {
-          '0%': { opacity: 0.2 },
-          '20%': { opacity: 1 },
-          '100% ': { opacity: 0.2 }
-        }
-      },
-      animation: {
-        fadeIn: 'fadeIn .3s ease-in-out',
-        carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
-      }
-    }
+    },
   },
-  future: {
-    hoverOnlyWhenSupported: true
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          'animation-delay': (value) => {
-            return {
-              'animation-delay': value
-            };
-          }
-        },
-        {
-          values: theme('transitionDelay')
-        }
-      );
-    })
-  ]
+  plugins: [],
 };
