48d64ad3bfaab83022475ffb8d51eae9d7f77b43
alyssariah <alyssariah@gmail.com>
2023-05-08 20:03:13 -0400

    change env variables

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.bigcommerce.com/stores/jo3a1dt1';
-const X_AUTH_TOKEN = 'axkiw2t6o9us7bcafi83lxcdzl3f65t';
+const API_URL = process.env.API_URL;
+const X_AUTH_TOKEN = process.env.X_AUTH_TOKEN;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
     'X-Auth-Token': X_AUTH_TOKEN,
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   },
 });
 
